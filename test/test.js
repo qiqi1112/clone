749de612787c0564be780f5be4a47f0517ff1ecc
琦琦 <1904710955@qq.com>
2023-02-09 21:22:02 +0800

    first commit

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var expect = require('expect.js')
+// var clone = require('../dist/index').clone;
+var clone = require('../src/index.js').clone;
+
+describe('function clone', function () {
+    describe('param data', function () {
+        it ('正确的测试用例', function () {
+            //基本数据类型
+            expect(clone('abc')).to.equal('abc');
+            // 数组
+            var arr = [1, [2]];
+            var cloneArr = clone(arr);
+            expect(cloneArr).not.to.equal(arr);
+            expect(cloneArr).to.eql(arr);
+            // 对象
+            var obj = {a:{b:1}};
+            var cloneObj = clone(obj);
+            expect(cloneObj).not.to.equal(obj);
+            expect(cloneObj).to.eql(obj);
+        });
+        it('边界值测试用例',function(){
+            expect(clone()).to.equal(undefined);
+            expect(clone(undefined)).to.equal(undefined);
+            expect(clone(null)).to.equal(null);
+        });
+    });
+});
